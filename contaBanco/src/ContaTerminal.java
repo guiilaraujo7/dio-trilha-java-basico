6825111ec3d9c0ae37b2b5aba2fbe777ce5a0016
guiilaraujo7 <gui_2009ar@hotmail.com>
2025-07-09 15:03:26 -0300

    Inserindo leitura e impressão de dados no banco

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner scanner =new Scanner(System.in);
+       System.out.println("olá, por favor digite seu nome");
+       String nome = scanner.next();
+       System.out.println("por favor, digite sua agencia");
+       int agencia = scanner.nextInt();
+       System.out.println("por favor, digite sua seu numero");
+       int numero = scanner.nextInt();    
+       System.out.println("por favor digite seu saldo");
+       float saldo = scanner.nextFloat();
+       System.out.printf("Olá %s, obrigado por criar um conta em nosso banco, sua agência é %s, conta %s e seu saldo %s já está disponível para saque", nome, agencia, numero, saldo);
+
     }
 }
